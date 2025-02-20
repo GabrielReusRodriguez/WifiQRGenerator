5ef8ad7c5e1a90ce2d95f3a82d1cc3b2475542a0
Gabriel Reus <Gabriel.Reus@gmail.com>
2025-02-20 23:16:28 +0100

    Arreglado el paso de parametros

--- scripts/wifi2QR.sh
+++ scripts/wifi2QR.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "/opt/apps/WifiQRGenerator/scripts/config.cfg"
 
-bash -c "${PYTHON_INTERPETER} ${HOME_FOLDER}/src/wifi2QR.py"
+bash -c "${PYTHON_INTERPETER} ${HOME_FOLDER}/src/wifi2QR.py $@"
